4e669c0e401992be78f4ba299e96147a5ff46b07
popshuv <ethannguyen4399@gmail.com>
2024-09-03 12:04:11 -0400

    added header file for class matrix

--- matrix.cpp
+++ matrix.cpp
@@ -1,84 +1,90 @@
-class Matrix {
+#include "matrix.h"
+#include <iostream>
 
-private:
-    int rows, cols;
-    double** data;
+// Constructor
+Matrix::Matrix(int rows, int cols) {
+    this->rows = rows;
+    this->cols = cols;
+    data = new double* [rows];
+    for (int i = 0; i < rows; ++i) {
+        data[i] = new double[cols];
+    }
+}
 
-public:
-    Matrix(int rows, int cols) {
-        this->rows = rows;
-        this->cols = cols;
-        data = new double* [rows];
-        for (int i = 0; i < rows; ++i) {
-            data[i] = new double[cols];
+// Copy constructor
+Matrix::Matrix(const Matrix& other) {
+    rows = other.rows;
+    cols = other.cols;
+    data = new double* [rows];
+    for (int i = 0; i < rows; ++i) {
+        data[i] = new double[cols];
+        for (int j = 0; j < cols; ++j) {
+            data[i][j] = other.data[i][j];
         }
     }
+}
 
-    Matrix(const Matrix& other) {
-        rows = other.rows;
-        cols = other.cols;
-        data = new double* [rows];
-        for (int i = 0; i < rows; ++i) {
-            data[i] = new double[cols];
-            for (int j = 0; j < cols; ++j) {
-                data[i][j] = other.data[i][j]; 
+// Destructor
+Matrix::~Matrix() {
+    for (int i = 0; i < rows; ++i) {
+        delete[] data[i];
+    }
+    delete[] data;
+}
+
+// Addition operator
+Matrix Matrix::operator+(const Matrix& other) {
+    Matrix result(rows, cols);
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            result.data[i][j] = data[i][j] + other.data[i][j];
+        }
+    }
+    return result;
+}
+
+// Subtraction operator
+Matrix Matrix::operator-(const Matrix& other) {
+    Matrix result(rows, cols);
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            result.data[i][j] = data[i][j] - other.data[i][j];
+        }
+    }
+    return result;
+}
+
+// Multiplication operator
+Matrix Matrix::operator*(const Matrix& other) {
+    Matrix result(rows, other.cols);
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < other.cols; ++j) {
+            result.data[i][j] = 0;
+            for (int k = 0; k < cols; ++k) {
+                result.data[i][j] += data[i][k] * other.data[k][j];
             }
         }
     }
+    return result;
+}
 
-    ~Matrix() {
-		for (int i = 0; i < rows; ++i) {
-			delete[] data[i];
-		}
-		delete[] data;
-	}
+// Element access (non-const)
+double& Matrix::at(int row, int col) {
+    return data[row][col];
+}
 
-    Matrix operator+(const Matrix& other) {
-		Matrix result(rows, cols);
-		for (int i = 0; i < rows; ++i) {
-			for (int j = 0; j < cols; ++j) {
-				result.data[i][j] = data[i][j] + other.data[i][j];
-			}
-		}
-		return result;
-	}
+// Element access (const)
+double Matrix::at(int row, int col) const {
+    return data[row][col];
+}
 
-    Matrix operator-(const Matrix& other) {
-        Matrix result(rows, cols);
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                result.data[i][j] = data[i][j] - other.data[i][j];
-            }
-        }
-        return result;
-    }
+// Get number of rows
+int Matrix::getRows() const {
+    return rows;
+}
 
-    Matrix operator*(const Matrix& other) {
-		Matrix result(rows, other.cols);
-		for (int i = 0; i < rows; ++i) {
-			for (int j = 0; j < other.cols; ++j) {
-				result.data[i][j] = 0;
-				for (int k = 0; k < cols; ++k) {
-					result.data[i][j] += data[i][k] * other.data[k][j];
-				}
-			}
-		}
-		return result;
-	}
+// Get number of columns
+int Matrix::getCols() const {
+    return cols;
+}
 
-    double& at(int row, int col) {
-        return data[row][col];
-    }
-
-    double at(int row, int col) const {
-        return data[row][col];
-    }
-
-    int getRows() const {
-        return rows;
-    }
-
-    int getCols() const {
-        return cols;
-    }
-};
--- matrix.h
+++ matrix.h
@@ -1,1 +1,23 @@
 #pragma once
+
+class Matrix {
+private:
+    int rows, cols;
+    double** data;
+
+public:
+    Matrix(int rows, int cols);
+    Matrix(const Matrix& other);
+    ~Matrix();
+
+    Matrix operator+(const Matrix& other);
+    Matrix operator-(const Matrix& other);
+    Matrix operator*(const Matrix& other);
+
+    double& at(int row, int col);
+    double at(int row, int col) const;
+
+    int getRows() const;
+    int getCols() const;
+};
+
